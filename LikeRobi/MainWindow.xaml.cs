6bf1c740e78d0087edf444833753f0f89407ea2e
motto <motto001@gmail.com>
2015-07-19 21:05:57 +0200

    alap2

--- LikeRobi/MainWindow.xaml.cs
+++ LikeRobi/MainWindow.xaml.cs
@@ -7,11 +7,23 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using mshtml;
+using HtmlAgilityPack;
+using RobiDll;
 
 namespace LikeRobi
 {
@@ -23,6 +35,7 @@ namespace LikeRobi
         public MainWindow()
         {
             InitializeComponent();
+            web1.LoadCompleted += web1_LoadCompleted;
         }
     }
 }
